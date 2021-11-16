4aa77ebe6160e79e42ebb8575881c7e7d62fa115
Ivan Karash <karash.www@gmail.com>
2021-11-16 14:42:21 +0300

    New

--- Algorithmic_Problems_Sharp/Program.cs
+++ Algorithmic_Problems_Sharp/Program.cs
@@ -6,119 +6,162 @@ using System.Text.RegularExpressions;
 
 namespace Algorithmic_Problems_Sharp
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Hello Algos!");
-			// Задача:
-			// Мое решение:
-			// Хорошее решение:
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Algos!");
+            // Задача:
+            // Мое решение:
+            // Хорошее решение:
 
 
-			//======================================================================
-			//======================================================================
-			// Задача: Установить, является ли вторая строка окончанием первой строки
+            //======================================================================
+            //======================================================================
+            // Задача: Установить, является ли вторая строка окончанием первой строки
 
-			//// Мое решение:
-			//bool Solution(string str, string ending)
-			//{
-			//	//ДОПОЛНИТЕЛЬНО: можно сначала перевернуть строки, чтобы было легче в цикле
-			//	//если второй аргумент длиннее первого - сразу вернуть фалс
-			//	//перебор первого на длину второго
-			//	//если все совпадает - тру, "все" проверяется счетчиком
+            //// Мое решение:
+            //bool Solution(string str, string ending)
+            //{
+            //	//ДОПОЛНИТЕЛЬНО: можно сначала перевернуть строки, чтобы было легче в цикле
+            //	//если второй аргумент длиннее первого - сразу вернуть фалс
+            //	//перебор первого на длину второго
+            //	//если все совпадает - тру, "все" проверяется счетчиком
 
-			//	if (ending.Length > str.Length)
-			//		return false;
+            //	if (ending.Length > str.Length)
+            //		return false;
 
-			//	var cnt = 0;
-			//	for (int i = 1; i <= ending.Length; i++)
-			//	{
-			//		if (str[str.Length - i] == ending[ending.Length - i])
-			//			cnt++;
-			//	}
+            //	var cnt = 0;
+            //	for (int i = 1; i <= ending.Length; i++)
+            //	{
+            //		if (str[str.Length - i] == ending[ending.Length - i])
+            //			cnt++;
+            //	}
 
-			//	if (cnt == ending.Length)
-			//		return true;
+            //	if (cnt == ending.Length)
+            //		return true;
 
-			//	return false;
-			//}
+            //	return false;
+            //}
 
-			//// Хорошее решение:
-			//bool Solution(string str, string ending)
-			//{
-			//	if (str.EndsWith(ending))
-			//		return true;
-			//	else return false;
-			//}
+            //// Хорошее решение:
+            //bool Solution(string str, string ending)
+            //{
+            //	if (str.EndsWith(ending))
+            //		return true;
+            //	else return false;
+            //}
 
-			//Console.WriteLine(Solution("abc", "bc"));  // returns true
-			//Console.WriteLine(Solution("abc", "d")); // returns false
-			//Console.WriteLine(Solution("ninja", "j")); //  returns true
+            //Console.WriteLine(Solution("abc", "bc"));  // returns true
+            //Console.WriteLine(Solution("abc", "d")); // returns false
+            //Console.WriteLine(Solution("ninja", "j")); //  returns true
 
-			//======================================================================
-			//======================================================================
-			// Задача: во входящей строке заменить каждую букву на ее позицию в алфавите
+            //======================================================================
+            //======================================================================
+            // Задача: во входящей строке заменить каждую букву на ее позицию в алфавите
 
-			// Мое решение:
-			//static string AlphabetPosition(string text)
-			//{
-			//	var preString = text.ToLower().Split(" ");
-			//	var charsPre = string.Join("", preString);
+            // Мое решение:
+            //static string AlphabetPosition(string text)
+            //{
+            //	var preString = text.ToLower().Split(" ");
+            //	var charsPre = string.Join("", preString);
 
-			//	var cleanString = new string(charsPre.Where(Char.IsLetter).ToArray());
+            //	var cleanString = new string(charsPre.Where(Char.IsLetter).ToArray());
 
-			//	var inputLetters = cleanString.ToCharArray();
+            //	var inputLetters = cleanString.ToCharArray();
 
-			//	var alphabetLetters = new char[26];
-			//	for (int i = 0, j = 97;  i < alphabetLetters.Length && j <= 122; i++, j++)
-			//	{
-			//		alphabetLetters[i] = Convert.ToChar(j);
-			//	}
+            //	var alphabetLetters = new char[26];
+            //	for (int i = 0, j = 97;  i < alphabetLetters.Length && j <= 122; i++, j++)
+            //	{
+            //		alphabetLetters[i] = Convert.ToChar(j);
+            //	}
 
-			//	var sequentialNumbers = new int[inputLetters.Length];
-			//	for (int i = 0; i < inputLetters.Length; i++)
-			//	{
-			//		for (int j = 0; j < alphabetLetters.Length; j++)
-			//		{
-			//			if (inputLetters[i] == alphabetLetters[j])
-			//				sequentialNumbers[i] = j + 1;
-			//		}
-			//	}
+            //	var sequentialNumbers = new int[inputLetters.Length];
+            //	for (int i = 0; i < inputLetters.Length; i++)
+            //	{
+            //		for (int j = 0; j < alphabetLetters.Length; j++)
+            //		{
+            //			if (inputLetters[i] == alphabetLetters[j])
+            //				sequentialNumbers[i] = j + 1;
+            //		}
+            //	}
 
-			//	sequentialNumbers = sequentialNumbers.Where(i => i != 0).ToArray();
+            //	sequentialNumbers = sequentialNumbers.Where(i => i != 0).ToArray();
 
-			//	string result = string.Join(" ", sequentialNumbers);
-			//	return result;
-			//}
+            //	string result = string.Join(" ", sequentialNumbers);
+            //	return result;
+            //}
+
+            //// Хорошее решение: ПРЕОБРАЗОВАТЬ ОДНО В ДРУГОЕ МОЖНО С ПОМОЩЬЮ SELECT, НЕ НАДО НИЧЕГО ВЫДУМЫВАТЬ;
+            //// Также можно было решить через Dictionary
+            //static string AlphabetPosition(string text)
+            //{
+            //	return string.Join(" ", text.ToLower().Where(c => char.IsLetter(c))
+            //										  .Select(c => "abcdefghijklmnopqrstuvwxyz".IndexOf(c) + 1)
+            //										  .ToArray());
+            //	//или
+            //	return string.Join(" ", text.ToLower().Where(c => char.IsLetter(c)).Select(c => c - 'a' + 1));
+
+            //}
+
+            //Assert.AreEqual("20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11", 
+            //	AlphabetPosition("The sunset sets at twelve o' clock."));
+
+            //======================================================================
+            //======================================================================
+
+            //string BreakCamelCase(string str)
+            //{
+            //	string inputString = str;
+
+            //	for (int i = 0; i < inputString.Length; i++)
+            //	{
+            //		if (Char.IsUpper(inputString[i]) && i != 0)
+            //		{
+
+            //			inputString = inputString.Insert(i, " ");
+            //			i++;
+            //		}
+            //	}
+
+            //	return inputString;
+            //}
+
+            //         Console.WriteLine(BreakCamelCase("CamelCaseZaz"));
 
 
-			// Хорошее решение: ПРЕОБРАЗОВАТЬ ОДНО В ДРУГОЕ МОЖНО С ПОМОЩЬЮ SELECT, НЕ НАДО НИЧЕГО ВЫДУМЫВАТЬ;
-			// Также можно было решить через Dictionary
-			static string AlphabetPosition(string text)
-			{
-				return string.Join(" ", text.ToLower().Where(c => char.IsLetter(c))
-													  .Select(c => "abcdefghijklmnopqrstuvwxyz".IndexOf(c) + 1)
-													  .ToArray());
-				//или
-				return string.Join(" ", text.ToLower().Where(c => char.IsLetter(c)).Select(c => c - 'a' + 1));
+            // Задача: Все нули в массиве перебросить в правую часть, сохраняя порядок остальных элементов
+           
+            
+           
+            int[] MoveZeroes(int[] arr)
+            {
+                // Мое решение:
+                List<int> newList = new List<int>();
+                int howMuchZeroes = 0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] != 0)
+                        newList.Add(arr[i]);
+                    else howMuchZeroes++;
+                }
 
-			}
+                for (int i = 0; i < howMuchZeroes; i++)
+                {
+                    newList.Add(0);
+                }
+                return newList.ToArray();
+                
+                // Хорошее решение:
+                return arr.OrderBy(x => x == 0).ToArray();
+            }
 
-			Assert.AreEqual("20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11", 
-				AlphabetPosition("The sunset sets at twelve o' clock."));
+            var arr = MoveZeroes(new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 });
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
 
-
-
-
-
-
-
-
-
-
-
-
-		}
-	}
+        }
+    }
 }
--- Tests_Sharp/UnitTest1.cs
+++ Tests_Sharp/UnitTest1.cs
@@ -5,111 +5,168 @@ using System.Linq;
 
 namespace Tests_Sharp
 {
-	public class Tests
-	{
-		[Test]
-		public void Test1()
-		{
-			string AlphabetPosition(string text)
-			{
-				//return string.Join(" ", text.ToLower().Where(c => char.IsLetter(c))
-				//.Select(c => "abcdefghijklmnopqrstuvwxyz".IndexOf(c) + 1)
-				//.ToArray());
-				//или
-				return string.Join(" ", text.ToLower().Where(c => char.IsLetter(c)).Select(c => c - 'a' + 1));
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            string AlphabetPosition(string text)
+            {
+                //return string.Join(" ", text.ToLower().Where(c => char.IsLetter(c))
+                //.Select(c => "abcdefghijklmnopqrstuvwxyz".IndexOf(c) + 1)
+                //.ToArray());
+                //или
+                return string.Join(" ", text.ToLower().Where(c => char.IsLetter(c)).Select(c => c - 'a' + 1));
 
-			}
+            }
 
-			Assert.AreEqual("20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11",
-				AlphabetPosition("The sunset sets at twelve o' clock."));
-		}
+            Assert.AreEqual("20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11",
+                AlphabetPosition("The sunset sets at twelve o' clock."));
+        }
 
-		[Test]
-		public void AreTheySame()
-		{
-			bool comp(int[] a, int[] b)
-			{
-				if (a.Length == 0 || b.Length == 0 || b.Length > a.Length)
-				{
-					return false;
-				}
-				else
-				{
-					List<int> notUsed = a.OfType<int>().ToList();
-					int cnt = 0;
-					foreach (var num in b)
-					{
-						if (a.Any(n => notUsed.Contains(n) && n == Math.Sqrt(num)))
-						{
-							cnt++;
-							notUsed.Remove((int)Math.Sqrt(num));
-						}
-					}
+        [Test]
+        public void AreTheySame()
+        {
+            bool comp(int[] a, int[] b)
+            {
+                if (a.Length == 0 || b.Length == 0 || b.Length > a.Length)
+                {
+                    return false;
+                }
+                else
+                {
+                    List<int> notUsed = a.OfType<int>().ToList();
+                    int cnt = 0;
+                    foreach (var num in b)
+                    {
+                        if (a.Any(n => notUsed.Contains(n) && n == Math.Sqrt(num)))
+                        {
+                            cnt++;
+                            notUsed.Remove((int)Math.Sqrt(num));
+                        }
+                    }
 
-					if (cnt == b.Length) return true;
-					else return false;
-				}
+                    if (cnt == b.Length) return true;
+                    else return false;
+                }
+            }
 
-			}
+            int[] a = new int[] { 121, 144, 19, 161, 19, 144, 19, 11 };
+            int[] b = new int[] { 11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19 };
 
-			int[] a = new int[] { 121, 144, 19, 161, 19, 144, 19, 11 };
-			int[] b = new int[] { 11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19 };
+            int[] c = new int[] { 121, 144, 19, 161, 19, 144, 19, 11 };
+            int[] d = new int[] { 121, 14641, 20736, 361, 25921, 361, 20736, 361 };
 
-			int[] c = new int[] { 121, 144, 19, 161, 19, 144, 19, 11 };
-			int[] d = new int[] { 121, 14641, 20736, 361, 25921, 361, 20736, 361 };
-
-			bool r = comp(a, b); // True
-			Assert.AreEqual(true, r);
-		}
-
-
-		[Test]
-		public void Converter()
-		{
-			string dnaToRna(string dna)
-			{
-				return dna.Replace("T", "U");
-			}
-
-			Assert.AreEqual("UUUU", dnaToRna("TTTT"));
-		}
-
-		[Test]
-		public void CountXY()
-		{
-			bool XO(string input)
-			{
-				// Мое решение:
-				var inputStr = input.ToLower();
-				var cntX = 0;
-				var cntY = 0;
-				foreach (var letter in inputStr)
-				{
-					if (letter == 'x')
-						cntX++;
-					if (letter == 'o')
-						cntY++;
-				}
-				if (cntX == cntY)
-					return true;
-				else return false;
-
-				// Хорошее решение:
-				return input.ToLower().Count(c => c == 'x') == input.ToLower().Count(c => c == 'o');
-			}
-			Assert.AreEqual(true, XO("xo"));
-			Assert.AreEqual(true, XO("xxOo"));
-			Assert.AreEqual(false, XO("xxxm"));
-			Assert.AreEqual(false, XO("Oo"));
-			Assert.AreEqual(false, XO("ooom"));
-		}
+            bool r = comp(a, b); // True
+            Assert.AreEqual(true, r);
+        }
 
 
+        [Test]
+        public void Converter()
+        {
+            string dnaToRna(string dna)
+            {
+                return dna.Replace("T", "U");
+            }
+
+            Assert.AreEqual("UUUU", dnaToRna("TTTT"));
+        }
+
+        [Test]
+        public void CountXY()
+        {
+            bool XO(string input)
+            {
+                // Мое решение:
+                var inputStr = input.ToLower();
+                var cntX = 0;
+                var cntY = 0;
+                foreach (var letter in inputStr)
+                {
+                    if (letter == 'x')
+                        cntX++;
+                    if (letter == 'o')
+                        cntY++;
+                }
+                if (cntX == cntY)
+                    return true;
+                else return false;
+
+                // Хорошее решение:
+                return input.ToLower().Count(c => c == 'x') == input.ToLower().Count(c => c == 'o');
+            }
+            Assert.AreEqual(true, XO("xo"));
+            Assert.AreEqual(true, XO("xxOo"));
+            Assert.AreEqual(false, XO("xxxm"));
+            Assert.AreEqual(false, XO("Oo"));
+            Assert.AreEqual(false, XO("ooom"));
+        }
+
+
+        [Test]
+        public void CamelCase()
+        {
+            string BreakCamelCase(string str)
+            {
+                string inputString = str;
+                for (int i = 0; i < inputString.Length; i++)
+                {
+                    if (Char.IsUpper(inputString[i]) && i != 0)
+                    {
+                        // Если после вставки пробела не сместить i на символ вправо, программа
+                        // будет бесконечно вставлять пробел перед выбранной буквой
+                        inputString = inputString.Insert(i, " ");
+                        i++;
+                    }
+                }
+
+                return inputString;
+            }
+            Assert.AreEqual("Camel Case", BreakCamelCase("CamelCase"));
+            Assert.AreEqual("xxio", BreakCamelCase("xxio"));
+            Assert.AreEqual("Hello World", BreakCamelCase("HelloWorld"));
+            Assert.AreEqual("Helloworld", BreakCamelCase("Helloworld"));
+            Assert.AreEqual("camel Case Caaase", BreakCamelCase("camelCaseCaaase"));
+        }
+
+
+        [Test]
+        public void MoveZeroesInArray()
+        {
+            int[] MoveZeroes(int[] arr)
+            {
+                // Мое решение:
+                List<int> newList = new List<int>();
+                int howMuchZeroes = 0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] != 0)
+                        newList.Add(arr[i]);
+                    else howMuchZeroes++;
+                }
+
+                for (int i = 0; i < howMuchZeroes; i++)
+                {
+                    newList.Add(0);
+                }
+                return newList.ToArray();
+
+                // Хорошее решение:
+                return arr.OrderBy(x => x == 0).ToArray();
+            }
+
+            Assert.AreEqual(new int[] { 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 }, MoveZeroes(new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 }));
+        }
 
 
 
 
 
 
-	}
+
+
+
+
+    }
 }
