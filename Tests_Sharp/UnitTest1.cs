6be51ee978b5258e7a6d936f3e4b3674e3dd56be
Ivan Karash <karash.www@gmail.com>
2022-01-13 12:59:59 +0300

    To previous commit

--- Tests_Sharp/UnitTest1.cs
+++ Tests_Sharp/UnitTest1.cs
@@ -1752,6 +1752,23 @@ namespace Tests_Sharp
                         }
                     }
                 }
+
+                ////Альтернативное решение
+                //void Swap(int[] arr, int a, int b)
+                //{
+                //    int temp = arr[a];
+                //    arr[a] = arr[b];
+                //    arr[b] = temp;
+                //}
+
+                //for (int i = 0; i < arr.Length; i += 2)
+                //{
+                //    if (i > 0 && arr[i - 1] > arr[i])
+                //        Swap(arr, i - 1, i);
+
+                //    if (i < arr.Length - 1 && arr[i] < arr[i + 1])
+                //        Swap(arr, i, i + 1);
+                //}
             }
 
             //Тестов нет
