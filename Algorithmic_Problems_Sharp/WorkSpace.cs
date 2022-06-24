ff88091e654d4e1e21c162a68fc6076d43d3197e
Ivan Karash <karash.www@gmail.com>
2022-06-24 13:55:02 +0300

    mid

--- Algorithmic_Problems_Sharp/WorkSpace.cs
+++ Algorithmic_Problems_Sharp/WorkSpace.cs
@@ -20,9 +20,23 @@ namespace Algorithmic_Problems_Sharp
 
         static void Main(string[] args)
         {
+            var colors = new string[] { "brown", "grey", "black", "red" };
+            var x = colors.Max(c => c.Length);
+            var z = colors.Count(c => c.Length > 3);
+            var y = colors.Count(c => c.Contains('e'));
 
 
-          
+            Console.WriteLine(x);
+            Console.WriteLine(z);
+            Console.WriteLine(y);
+
+
+            // Найти самое длинное слово содержащее хотя бы одну букву "e"
+            var words = new string[] { "brown", "grey", "black", "red", "redmond", "error", "adbcdtrdgqsfd", "advantage" };
+            var target = words.Aggregate((target, i) => i.Length > target.Length && i.Contains('e') ? target = i : target);
+            Console.WriteLine(target);
+
+            Console.WriteLine(new string(words[0].ToCharArray().Reverse().ToArray()));
 
 
         }
