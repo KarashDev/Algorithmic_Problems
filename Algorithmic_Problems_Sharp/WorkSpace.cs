7195a312400c21e04aa85833f6f6a6f58f3ac7d1
Ivan Karash <Karash.www@gmail.com>
2022-10-05 18:37:39 +0300

    Децл по поиску элемента

--- Algorithmic_Problems_Sharp/WorkSpace.cs
+++ Algorithmic_Problems_Sharp/WorkSpace.cs
@@ -62,30 +62,68 @@ namespace Algorithmic_Problems_Sharp
         static void Main(string[] args)
         {
 
-            Person person;
+            //Person person;
 
-            try
+            //try
+            //{
+            //    person = new Person("", 0);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+            //try
+            //{
+            //    person = new Person("Boba", 0);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+            //person = new Person("Ivan", 28);
+
+            //Console.WriteLine(person.Name);
+            //Console.WriteLine(person.Age);
+
+
+
+            //В первой строке задается одно натуральное число N, не превосходящее 10001000.
+            //Во второй строке вводятся N целых чисел, каждое из которых по модулю не превосходит 10001000.
+            //В третьей строке содержится одно целое число x, не превосходящее по модулю 10001000.
+
+            // Линейный поиск через Aggregate
+            var inputCount = Convert.ToInt32(Console.ReadLine());
+
+            var numbersStrings = Console.ReadLine().Split(" ");
+            var numbers = Array.ConvertAll(numbersStrings, n => Convert.ToInt32(n));
+
+            var numToFind = Convert.ToInt32(Console.ReadLine());
+
+
+            int LinearSearch(int[] numbers, int x) => numbers.Aggregate(0, (cnt, i) => i == x ? cnt + 1 : cnt);
+
+            //Console.WriteLine(LinearSearch(numbers, numToFind));
+
+
+
+            // То же самое, только надо вывести порядковые номера всех найденных чисел X (i + 1)
+            List<int> FindIndexes(List<int> numbers, int x)
             {
-                person = new Person("", 0);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
+                List<int> matches = new List<int>();
+
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    if (numbers[i] == x)
+                        matches.Add(i + 1);
+                }
+                return matches;
             }
 
-            try
-            {
-                person = new Person("Boba", 0);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            var result = string.Join(" ", FindIndexes(numbers.ToList(), numToFind));
 
-            person = new Person("Ivan", 28);
-
-            Console.WriteLine(person.Name);
-            Console.WriteLine(person.Age);
+            Console.WriteLine(result);
 
 
         }
