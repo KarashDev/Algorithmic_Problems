511f2a7e8965606ed374606bed5b25e03db500fb
Ivan Karash <karash.www@gmail.com>
2022-10-06 13:57:31 +0300

    FindNearestNumber

--- Algorithmic_Problems_Sharp/WorkSpace.cs
+++ Algorithmic_Problems_Sharp/WorkSpace.cs
@@ -11,6 +11,8 @@ using System.Diagnostics;
 using System.Threading;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
+using System.Collections.Immutable;
+using Microsoft.VisualBasic;
 
 namespace Algorithmic_Problems_Sharp
 {
@@ -89,41 +91,101 @@ namespace Algorithmic_Problems_Sharp
 
 
 
-            //В первой строке задается одно натуральное число N, не превосходящее 10001000.
-            //Во второй строке вводятся N целых чисел, каждое из которых по модулю не превосходит 10001000.
-            //В третьей строке содержится одно целое число x, не превосходящее по модулю 10001000.
+            ////В первой строке задается одно натуральное число N, не превосходящее 10001000.
+            ////Во второй строке вводятся N целых чисел, каждое из которых по модулю не превосходит 10001000.
+            ////В третьей строке содержится одно целое число x, не превосходящее по модулю 10001000.
 
-            // Линейный поиск через Aggregate
-            var inputCount = Convert.ToInt32(Console.ReadLine());
+            //// Линейный поиск через Aggregate
+            //var inputCount = Convert.ToInt32(Console.ReadLine());
 
-            var numbersStrings = Console.ReadLine().Split(" ");
-            var numbers = Array.ConvertAll(numbersStrings, n => Convert.ToInt32(n));
+            //var numbersStrings = Console.ReadLine().Split(" ");
+            //var numbers = Array.ConvertAll(numbersStrings, n => Convert.ToInt32(n));
 
-            var numToFind = Convert.ToInt32(Console.ReadLine());
-
-
-            int LinearSearch(int[] numbers, int x) => numbers.Aggregate(0, (cnt, i) => i == x ? cnt + 1 : cnt);
-
-            //Console.WriteLine(LinearSearch(numbers, numToFind));
+            //var numToFind = Convert.ToInt32(Console.ReadLine());
 
 
+            //int LinearSearch(int[] numbers, int x) => numbers.Aggregate(0, (cnt, i) => i == x ? cnt + 1 : cnt);
 
-            // То же самое, только надо вывести порядковые номера всех найденных чисел X (i + 1)
-            List<int> FindIndexes(List<int> numbers, int x)
+            ////Console.WriteLine(LinearSearch(numbers, numToFind));
+
+
+
+            //// То же самое, только надо вывести порядковые номера всех найденных чисел X (i + 1)
+            //List<int> FindIndexes(List<int> numbers, int x)
+            //{
+            //    List<int> matches = new List<int>();
+
+            //    for (int i = 0; i < numbers.Count; i++)
+            //    {
+            //        if (numbers[i] == x)
+            //            matches.Add(i + 1);
+            //    }
+            //    return matches;
+            //}
+
+            //var result = string.Join(" ", FindIndexes(numbers.ToList(), numToFind));
+
+            //Console.WriteLine(result);
+
+
+
+
+            // Если есть элементы больше заданного - нахожу разницу между минимальным бОльшим и заданным
+            // Если есть элементы меньше заданного - нахожу разницу между максимальным меньшим и заданным
+            // Если истинны оба варианта: сравнить эти две разницы, выбрать элемент с минимальной
+
+
+
+
+
+
+            //var inputCount = Convert.ToInt32(Console.ReadLine());
+
+            //var numbersStrings = Console.ReadLine().Split(" ");
+            //var numbers = Array.ConvertAll(numbersStrings, n => Convert.ToInt32(n));
+
+            //var numToFind = Convert.ToInt32(Console.ReadLine());
+
+
+
+            int FindNearestNumber(int[] numbers, int x)
             {
-                List<int> matches = new List<int>();
+                var numsOrderedByProximity = numbers.OrderBy(n => Math.Abs(n - x)).ToArray();
 
-                for (int i = 0; i < numbers.Count; i++)
+                var indexesToPosDiffs = new Dictionary<int, int>();
+                for (int i = 0; i < numsOrderedByProximity.Length; i++)
                 {
-                    if (numbers[i] == x)
-                        matches.Add(i + 1);
+                    indexesToPosDiffs.TryAdd(numbers[i], Math.Abs(numbers[i] - x));
                 }
-                return matches;
+
+                var smallestValues = indexesToPosDiffs.Where(pair => pair.Value == indexesToPosDiffs.Min(p => p.Value));
+
+                if (smallestValues.Count() > 0)
+                    return smallestValues.OrderBy(p => p.Key).Last().Key;
+                else
+                    return smallestValues.First().Key;
             }
 
-            var result = string.Join(" ", FindIndexes(numbers.ToList(), numToFind));
+            //Console.WriteLine(FindNearestNumber(numbers, numToFind));
+            //Console.WriteLine(FindNearestNumber(new int[] { 2, 10, 5 }, 6));
+            //Console.WriteLine(FindNearestNumber(new int[] { 1, 4, 7, 10 }, 1));
+            //Console.WriteLine(FindNearestNumber(new int[] { -1000, 1000 }, 0));
+            //Console.WriteLine(FindNearestNumber(new int[] { 2, 3, 5, 6 }, 4));
+            //Console.WriteLine(FindNearestNumber(new int[] { 1000, -1000 }, 0));
+            Console.WriteLine(FindNearestNumber(new int[] { -1000, -1000 }, 0));
 
-            Console.WriteLine(result);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         }
