3df733f95795356ba024a043d836eb60d8c24aa6
Ivan Karash <Karash.www@gmail.com>
2022-11-23 17:02:46 +0300

    Задачки для чата

---
+++ Algorithmic_Problems_Sharp/ProblemsForChat.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithmic_Problems_Sharp
+{
+    internal class ProblemsForChat
+    {
+        //// Задача 1
+        //[Flags]
+        //public enum Status
+        //{
+        //    Funny = 0x01,
+        //    Hilarious = 0x02,
+        //    Boring = 0x04,
+        //    Cool = 0x08,
+        //    Interesting = 0x10,
+        //    Informative = 0x20,
+        //    Error = 0x40
+        //}
+        //var code = 24;
+        //var q = (Status)code;
+        //Console.WriteLine(String.Format("This Quiz is: {0}", (Status)code));
+
+
+        //// Задача 2
+        //var arr1 = Enumerable.Range(0, 100).Select(x => x * 2).ToArray();
+        //var arr2 = arr1.TakeWhile(x => x % 2 == 0).Except(arr1).Select(x => x).ToArray();
+
+        //Console.WriteLine(Enumerable.SequenceEqual(arr1, arr2));
+
+
+        //// Задача 3
+        /////static async void IterateAsync(int[] nums)
+        //{
+        //    await new Task(() => DoIterations(nums));
+        //}
+
+        //static void DoIterations(int[] nums)
+        //{
+        //    foreach (var num in nums)
+        //    {
+        //        Console.WriteLine(num);
+        //    }
+        //}
+
+        //static async Task Main(string[] args)
+        //{
+
+        //    var nums = new int[500];
+        //    for (int i = 0, j = 1; i < nums.Length; i++, j++)
+        //    {
+        //        nums[i] = j;
+        //    }
+
+        //    IterateAsync(nums);
+
+        //    for (int i = 0; i < 500; i++)
+        //    {
+        //        Console.WriteLine("Main method");
+        //    }
+
+        //}
+
+
+        //// Задача 4
+        //string[] strings = new string[] { "a", "b", "c", "d", "e", "f", "g" };
+        //var output = strings.Select(s => Array.IndexOf(strings, s) % 2 == 0 ? s += " marked" : "");
+
+        //foreach (var str in output)
+        //{
+        //    Console.Write(str + " ");
+        //}
+
+        //"a marked b c marked d e marked f g marked"
+        //"a marked  c marked  e marked  g marked"
+        //"a c e g"
+        //"b marked  d marked  f marked"
+
+
+
+
+    }
+}
--- Algorithmic_Problems_Sharp/WorkSpace.cs
+++ Algorithmic_Problems_Sharp/WorkSpace.cs
@@ -15,20 +15,45 @@ using System.Collections.Immutable;
 using Microsoft.VisualBasic;
 using CommandLine.Text;
 using Microsoft.Diagnostics.Tracing.Parsers.JSDumpHeap;
+using Microsoft.Diagnostics.Tracing.Parsers.ApplicationServer;
+using System.Net.WebSockets;
+using NUnit.Framework.Constraints;
+using System.Reflection;
+using System.Drawing;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Algorithmic_Problems_Sharp
 {
+#pragma warning disable
+
+
     class WorkSpace
     {
+     
 
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            
 
-          
+
+            
 
 
-         
+
+
+
+
+
+
         }
+
+
+
+
+
+
     }
 }
